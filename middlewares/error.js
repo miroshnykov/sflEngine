31ae732a870f28441e5fe58bf89b08cfd4855cd2
miroshnykov <dmytro.miroshnykov@adsurge.com>
2020-12-21 23:54:20 -0500

    AL-479 customLpRules

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,5 +1,5 @@
 module.exports = (err, req, res, next) => {
-    console.log('Error', {message: err.message, originUrl: req.originalUrl})
+    console.log('Error:', {message: err.message, originUrl: req.originalUrl})
     res.status(err.status || 500)
-    res.json({message: err.message ? err.message : 'API is not available, please try again later.'})
+    res.json({message: err.message ? err.message : 'Some errors.'})
 };
