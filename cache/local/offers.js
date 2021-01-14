937e315f9471b6196f1062cb6920ecdbe11631d2
miroshnykov <dmytro.miroshnykov@adsurge.com>
2021-01-14 13:36:48 -0500

    AL-515 fix route getRecipeData?offerId=6

--- cache/local/offers.js
+++ cache/local/offers.js
@@ -31,15 +31,15 @@ const setOffers = async () => {
 
         let file = config.recipe.offers
         let fileSizeInfo_ = await getDataCache('fileSizeInfo_')
-        if (!fileSizeInfo_){
+        if (!fileSizeInfo_) {
             logger.info(`Offer not define size in redis`)
             return
         }
         let size = getFileSize(file) || 0
         logger.info(`fileSizeInfo_offers:${fileSizeInfo_.offer}`)
-        if (size === fileSizeInfo_.offer){
+        if (size === fileSizeInfo_.offer) {
             logger.info(`Size of OFFERS the same lets add to redis  `)
-        } else{
+        } else {
             logger.info(`Size of recipe file OFFERS is different, need to reSend file from sfl-offer`)
             metrics.influxdb(200, `fileDifferentOffer_-${hostname}`)
             return
@@ -64,7 +64,7 @@ const setOffers = async () => {
         let jsonStream = JSONStream.parse('*')
         stream.pipe(gunzip).pipe(jsonStream)
         jsonStream.on('data', async (item) => {
-            if (!item.offerId){
+            if (!item.offerId) {
                 metrics.influxdb(500, `setOffersEmpty`)
                 return
             }
@@ -80,43 +80,43 @@ const setOffers = async () => {
         metrics.influxdb(500, `setOffersError`)
     }
 }
-//
-// const setData = async (key, body) => {
-//
-//     try {
-//         // console.log(`setData key:${key}:`, body)
-//         await setDataCache(key, JSON.parse(body))
-//
-//     } catch (e) {
-//         catchHandler(e, 'setDataError')
-//         metrics.influxdb(500, `setDataError`)
-//     }
-// }
-//
-// const getData = async (key) => {
-//
-//     try {
-//         // console.log('getData:', key)
-//         return await getDataCache(`${key}`)
-//
-//     } catch (e) {
-//         catchHandler(e, 'getDataError')
-//         metrics.influxdb(500, `getDataError`)
-//     }
-// }
-//
-// const delData = async (key) => {
-//
-//     try {
-//
-//         // console.log('delData:', key)
-//         await delDataCache(`${key}`)
-//
-//     } catch (e) {
-//         catchHandler(e, 'delDataError')
-//         metrics.influxdb(500, `delDataError`)
-//     }
-// }
+
+const setData = async (key, body) => {
+
+    try {
+        // console.log(`setData key:${key}:`, body)
+        await setDataCache(key, JSON.parse(body))
+
+    } catch (e) {
+        catchHandler(e, 'setDataError')
+        metrics.influxdb(500, `setDataError`)
+    }
+}
+
+const getData = async (key) => {
+
+    try {
+        // console.log('getData:', key)
+        return await getDataCache(`${key}`)
+
+    } catch (e) {
+        catchHandler(e, 'getDataError')
+        metrics.influxdb(500, `getDataError`)
+    }
+}
+
+const delData = async (key) => {
+
+    try {
+
+        // console.log('delData:', key)
+        await delDataCache(`${key}`)
+
+    } catch (e) {
+        catchHandler(e, 'delDataError')
+        metrics.influxdb(500, `delDataError`)
+    }
+}
 
 const setCampaigns = async () => {
 
@@ -124,7 +124,7 @@ const setCampaigns = async () => {
         let file = config.recipe.campaigns
 
         let fileSizeInfo_ = await getDataCache('fileSizeInfo_')
-        if (!fileSizeInfo_){
+        if (!fileSizeInfo_) {
             logger.info(`Campaign not define size in redis`)
             return
         }
@@ -133,9 +133,9 @@ const setCampaigns = async () => {
         logger.info(`sizeFile:`, size)
 
         logger.info(`fileSizeInfo_campaigns:${fileSizeInfo_.campaign}`)
-        if (size === fileSizeInfo_.campaign){
+        if (size === fileSizeInfo_.campaign) {
             logger.info(`Size the same lets add to redis  `)
-        } else{
+        } else {
             logger.info(`Size of recipe file campaigns is different , need to reSend file from sfl-offer`)
             metrics.influxdb(200, `fileDifferentCampaigns_-${hostname}`)
             return
@@ -160,7 +160,7 @@ const setCampaigns = async () => {
         let jsonStream = JSONStream.parse('*')
         stream.pipe(gunzip).pipe(jsonStream)
         jsonStream.on('data', async (item) => {
-            if (!item.campaignId){
+            if (!item.campaignId) {
                 metrics.influxdb(500, `setCampaignsEmpty`)
                 return
             }
@@ -182,7 +182,7 @@ const setCampaigns = async () => {
 module.exports = {
     setOffers,
     setCampaigns,
-    // getData,
+    getData,
     sqsProcessing
 
 }
