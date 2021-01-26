99073eec69f85fc843ee41c441683874cbe59c53
miroshnykov <dmytro.miroshnykov@adsurge.com>
2021-01-26 15:22:37 -0500

    get targeting from sfl-offers project , deprecated sfl-cache

--- cache/local/targeting.js
+++ cache/local/targeting.js
@@ -1,36 +1,36 @@
 const {catchHandler} = require('../../middlewares/catchErr')
 const {getDataCache, setDataCache} = require('../redis')
-const {getTargetingApi} = require('../api/targeting')
+// const {getTargetingApi} = require('../api/targeting')
 const metrics = require('../../metrics')
 
 const getTargetingLocal = async () => {
 
     try {
-        return await getDataCache(`targetingLocal`)
+        return await getDataCache(`targetingInfo_`)
     } catch (e) {
         catchHandler(e, 'getTargetingLocal')
         metrics.influxdb(500, `getTargetingLocalError`)
     }
 }
 
-const setTargetingLocal = async () => {
-
-    try {
-        let targeting = await getTargetingApi()
-        if (targeting) {
-            await setDataCache('targetingLocal', targeting)
-        }
-        return targeting
-
-    } catch (e) {
-        catchHandler(e, 'setTargetingLocal')
-        metrics.influxdb(500, `setTargetingLocalError`)
-    }
-}
+// const setTargetingLocal = async () => {
+//
+//     try {
+//         let targeting = await getTargetingApi()
+//         if (targeting) {
+//             await setDataCache('targetingLocal', targeting)
+//         }
+//         return targeting
+//
+//     } catch (e) {
+//         catchHandler(e, 'setTargetingLocal')
+//         metrics.influxdb(500, `setTargetingLocalError`)
+//     }
+// }
 
 
 module.exports = {
     getTargetingLocal,
-    setTargetingLocal
+    // setTargetingLocal
 }
 
