a50d4b16a09a7f41d33a17708cf2a75851761806
miroshnykov <dmytro.miroshnykov@adsurge.com>
2021-02-16 09:16:58 -0500

    sqs test config

--- routes/offers.js
+++ routes/offers.js
@@ -317,6 +317,8 @@ const sqsConversionTypeCmp = async (params) => {
     // console.log(obj)
     logger.info(`Added to SQS Conversion Type Cmp, Body:${JSON.stringify(obj)}`)
     let sqsData = await sendMessageToQueue(obj)
+    params.sendTOSQS = sqsData
+    params.sendTOSQSBody = obj
     // console.log(`Added update to redis sqs:${JSON.stringify(sqsData)}`)
 
 }
